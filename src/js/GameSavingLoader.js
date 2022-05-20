47eb92e5fb90c8908d6a3eb5995643e198044b64
Yashchenko Victor <Andrey@Komp11.local>
2022-05-20 21:38:03 +0300

    trying

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -4,12 +4,11 @@ import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
 
-  static load() {
-    return (async () => {
-      const a = await read();
-      const b = await json(a);
-      const obj = await JSON.parse(b);
-      return new GameSaving(obj.id, obj.created, obj.userInfo);
-    })();
+  static async load() {
+    const a = await read();
+    const b = await json(a);
+    const obj = await JSON.parse(b);
+    return new GameSaving(obj.id, obj.created, obj.userInfo);
+    ;
   }
 }
