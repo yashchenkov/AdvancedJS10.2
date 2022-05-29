7642843b07979e3116717589273a338109712187
Yashchenko Victor <Andrey@Komp11.local>
2022-05-29 20:02:28 +0300

    fixing 2

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -2,9 +2,9 @@ import GameSavingLoader from '../js/GameSavingLoader';
 
 test('testing GameSavingLoader result', async () => {
   const obj = await GameSavingLoader.load();
-  expect(JSON.stringify(obj)).toEqual(JSON.stringify({
+  expect(obj).toEqual({
 	  id: 9,
 	  created: 1546300800,
 	  userInfo: { id: 1, name: 'Hitman', level: 10, points: 2000 }
-  }));
+  });
 });
